0d6ed0aa3a791c26506c97a46aec4d36388dbdf6
unknown <gabrielle.christiando@binus.ac.id>
2026-02-13 12:50:34 +0700

    fix scroll

--- frontend/src/mobile - supervisor/MobileLayout.tsx
+++ frontend/src/mobile - supervisor/MobileLayout.tsx
@@ -67,8 +67,7 @@ const MobileLayout: React.FC<MobileLayoutProps> = ({ children, title = "Dashboar
                 <div className="h-24 w-full shrink-0"></div>
 
                 {/* Main Content */}
-                {/* if !allowScroll: Fill remaining height exactly and flex-col for children to manage scroll */}
-                <main className={`bg-gray-50 rounded-t-[30px] min-h-[calc(100vh-6rem)] w-full px-6 py-6 shadow-[0_-10px_40px_rgba(0,0,0,0.1)] relative z-20`}>
+                <main className={`bg-gray-50 rounded-t-[30px] w-full px-6 py-6 shadow-[0_-10px_40px_rgba(0,0,0,0.1)] relative z-20 ${allowScroll ? 'min-h-[calc(100vh-6rem)]' : 'h-[calc(100vh-6rem)] overflow-hidden flex flex-col'}`}>
                     {children}
 
                     {/* Bottom Padding only needed for scrollable main */}
